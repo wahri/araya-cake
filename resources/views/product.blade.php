2cfc9bfe26d5c73d40a47fbc2378d166c3fae43c
WnB <wahyubahri1212@gmail.com>
2023-10-19 07:24:45 +0700

    perbaiki view category

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -4,42 +4,42 @@
 @endpush
 
 @section('content')
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="search__container">
-                        <h1 class="text-center h1-lg araya-color">
-                            Cari cake kesukaan mu di Araya
-                        </h1>
-                    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="search__container">
+                    <h1 class="text-center h1-lg araya-color">
+                        Cari cake kesukaan mu di Araya
+                    </h1>
                 </div>
             </div>
-
         </div>
 
-        <div id="promo-3" class="pt-50 promo-section division">
-            <div class="container">
-                <div class="row d-flex align-items-center">
+    </div>
 
-                    @foreach ($categoryProduct as $category)
-                    <div class="col-md-4">
-                        @if ($category->image)
-                        <a href="{{ route('categoryProduct', $category->slug) }}">
-                            <div class="pbox-3 mb-30">
-                                <div class="hover-overlay"> 
-                                    <img class="img-fluid lazyload" data-src="{{ asset('images/' . $category->image->name) }}" alt="{{ $category->name }}" />
-                                </div> 
-                            </div>
-                        </a>
-                            
-                        @endif
-                    </div>
-                        
-                    @endforeach
+    <div id="promo-3" class="pt-50 promo-section division">
+        <div class="container">
+            <div class="row d-flex align-items-center">
+
+                @foreach ($categoryProduct as $category)
+                    @if ($category->image)
+                        <div class="col-md-4">
+                            <a href="{{ route('categoryProduct', $category->slug) }}">
+                                <div class="pbox-3 mb-30">
+                                    <div class="hover-overlay">
+                                        <img class="img-fluid lazyload"
+                                            data-src="{{ asset('images/' . $category->image->name) }}"
+                                            alt="{{ $category->name }}" />
+                                    </div>
+                                </div>
+                            </a>
+
+                        </div>
+                    @endif
+                @endforeach
 
 
-                </div>    <!-- End row -->		
-            </div>	   <!-- End container -->	
-        </div>	
-
+            </div> <!-- End row -->
+        </div> <!-- End container -->
+    </div>
 @endsection
